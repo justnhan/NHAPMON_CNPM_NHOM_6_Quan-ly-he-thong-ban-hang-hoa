df2abea4f24500e1f96e046e5d9abd995d595a32
Justnhan <nhandz1252006@gmail.com>
2025-11-27 20:32:44 +0700

    Update login messages to correct Vietnamese phrases

--- Đăng nhập-nhân.py
+++ Đăng nhập-nhân.py
@@ -1,27 +1,27 @@
 ad="NHAN"
 ps= "123"
-print("Chào m?ng b?n dã d?n!")
+print("Chao mung ban da den!")
 print("---------------------------------------------")
 for i in range (3):
-    print ("M?i b?n nh?p tên dang nh?p:")
+    print ("Moi ban nhap ten dang nhap:")
     a= input()
-    print("m?i b?n nh?p m?t kh?u:")
+    print("moi ban nhap mat khau:")
     b= input()
     if a==ad:
         for j in range (3):
             if b==ps:
                 flag=True
-                print ("Chào m?ng b?n dã d?n v?i nhà camon!")
+                print ("Chao mung ban da den voi nha camon!")
                 break
             else:
                 flag=False
-                print("sai m?t kh?u, vui lòng nh?p l?i!")
+                print("sai mat khau, vui long nhap lai!")
                 b=input()
         if flag==True:
             break
         else:
-            print("Sai quá nhi?u l?n, dã h?t lu?t nh?p! Vui lòng t?o l?i.")
+            print("sai qua nhieu lan, ban da het luot nhap! Vui long tao lai.")
             break
     else:
-        print("Tài kho?n không t?n t?i, vui lòng nh?p l?i!")
-print("chưa được sửa!")
+        print("Tai khoan khong ton tai! Vui long dang nhap lai")
+print("đã sửa!")
