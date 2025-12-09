181035a2a247b4ff8ad256cd8ca0da1a0a5d2440
DangDan111 <dangdan0404@gmail.com>
2025-12-09 08:11:49 +0700

    login

--- Hệ thống.py
+++ Hệ thống.py
@@ -1,3 +1,22 @@
+def login():
+    print("\n--- ĐĂNG NHẬP ---")
+    username = input("Tên tài khoản: ").strip()
+    password = input("Mật khẩu: ").strip()
+
+    if username not in users:
+        print("❌ Tài khoản không tồn tại!")
+        return
+
+    if users[username]["password"] != password:
+        print("❌ Sai mật khẩu!")
+        return
+
+    print("✅ Đăng nhập thành công!")
+
+    if users[username]["role"] == "buyer":
+        buyer_menu(username)
+    else:
+        seller_menu(username)
 def main():
     while True:
         print("\n=== MENU CHÍNH ===")
