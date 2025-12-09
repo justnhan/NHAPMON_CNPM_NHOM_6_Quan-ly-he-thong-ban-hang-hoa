dcfce7169bffc41fa10e936a3732e5a2c9f4e9fd
DangDan111 <dangdan0404@gmail.com>
2025-12-09 08:22:37 +0700

    register

--- Hệ thống.py
+++ Hệ thống.py
@@ -1,3 +1,46 @@
+def register():
+    print("\n--- ĐĂNG KÝ TÀI KHOẢN ---")
+
+    username = input("Tên tài khoản: ").strip()
+    if username in users:
+        print("❌ Tài khoản đã tồn tại!")
+        return
+
+    email = input("Email: ").strip()
+    phone = input("Số điện thoại: ").strip()
+
+    password = input("Mật khẩu: ").strip()
+    repass = input("Nhập lại mật khẩu: ").strip()
+
+    if password != repass:
+        print("❌ Mật khẩu không khớp!")
+        return
+
+    print("\nLoại tài khoản:")
+    print("1. Người bán")
+    print("2. Người mua")
+    role_choice = input("Chọn (1/2): ").strip()
+
+    role = "seller" if role_choice == "1" else "buyer"
+def login():
+    print("\n--- ĐĂNG NHẬP ---")
+    username = input("Tên tài khoản: ").strip()
+    password = input("Mật khẩu: ").strip()
+
+    if username not in users:
+        print("❌ Tài khoản không tồn tại!")
+        return
+
+    if users[username]["password"] != password:
+        print("❌ Sai mật khẩu!")
+        return
+
+    print("✅ Đăng nhập thành công!")
+
+    if users[username]["role"] == "buyer":
+        buyer_menu(username)
+    else:
+        seller_menu(username)
 def main():
     while True:
         print("\n=== MENU CHÍNH ===")
