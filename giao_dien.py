405178ef6196f860527e1ca01ab002613d45228e
justnhan <nhandz1252006@gmail.com>
2025-12-19 09:36:59 +0700

    nothing

--- giao_dien.py
+++ giao_dien.py
@@ -84,7 +84,7 @@ def buyer_menu(username):
             tiep_tuc()
             # gọi hàm tương ứng
         elif choice == "2":
-            search_product()
+            search_product(username)
             tiep_tuc()
         elif choice == "3":
             view_cart(username)
